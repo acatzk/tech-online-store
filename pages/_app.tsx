46a40cd59f9cd65f405e4775d748506e9df31ce4
acatzk <joshuaimalay@gmail.com>
2021-07-21 22:38:11 +0800

    🎉 Initial next and tailwind setup

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import './../styles/tailwind.css'
+import type { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+import { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Tech Online Store</title>
+      </Head>
+      <div className="text-2xl font-bold text-purple-600">Hi Gayle</div>
+    </>
+  )
+}
+
+export default Index
