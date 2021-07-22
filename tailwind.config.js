a5767aa1df2ec93d45a60c2f7be402f9f873f367
acatzk <joshuaimalay@gmail.com>
2021-07-22 11:22:31 +0800

    ✨ FEATURE: Initial mobile header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,32 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'color-1': '#F5F7FF',
+        'color-2': '#0074BE',
+        'color-3': '#0156FF',
+        'color-4': '#01A4FF',
+        'color-5': '#8C8C8C',
+        'color-6': '#CCCCCC',
+        'color-7': '#EBEBEB',
+        'color-9': '#C94D3F',
+        'color-10': '#78A962',
+        'color-11': '#666666'
       }
     }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
