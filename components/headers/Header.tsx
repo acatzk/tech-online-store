ce41eb73488f8cc81c381f959c86ae8940ec3a57
acatzk <joshuaimalay@gmail.com>
2021-07-28 20:55:28 +0800

    ✨ FEATURE: Add hover dropdown menu

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,13 +1,15 @@
 import dynamic from 'next/dynamic'
+import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { sidebarLinks } from 'mock/index'
 import { FiInstagram } from 'react-icons/fi'
 import { RiCloseLine } from 'react-icons/ri'
+import { Transition } from '@headlessui/react'
 import AccountDropdown from './AccountDropdown'
 import ScheduleDropdown from './ScheduleDropdown'
 import { RiFacebookBoxFill } from 'react-icons/ri'
-import { BiChevronDown, BiSearch } from 'react-icons/bi'
 import { Logo, Menu, Cart, User, LogoWide } from 'utils/Icons'
+import { BiChevronDown, BiSearch, BiChevronRight } from 'react-icons/bi'
 
 const Sidebar = dynamic(() => import('./Sidebar'), {
   ssr: false,
@@ -23,43 +25,19 @@ const Header: React.FC = () => {
     <React.Fragment>
       <header className="sticky top-0 overflow-x-hidden transition-all ease-in-out duration-300 z-10">
         {/* This is schedule and contact us links */}
-        <section className="text-xs font-semibold bg-black text-white">
-          <div className="container flex items-center justify-between px-4 lg:px-0 py-1">
-            <div className="ml-16 lg:ml-0">
-              <button
-                onClick={() => setIsSchedule((prev) => !prev)}
-                className="inline-flex items-center rounded-full px-1 py-0.5 border focus:border-color-5
-                  border-transparent hover:border-color-5 space-x-1 transition ease-in-out duration-150"
-              >
-                <span className="text-color-5 line-clamp-1">Mon-Thu:</span>
-                <span className="line-clamp-1">9:00 AM - 5:30 PM</span>
-                <BiChevronDown className="w-4 h-4" />
-              </button>
-              {/* This is where SchduleMenuToggle shows */}
-              {isSchedule && <ScheduleDropdown setIsSchedule={setIsSchedule} />}
-            </div>
-            <div className="flex items-center font-medium space-x-2">
-              <h1 className="text-[#ACACAC] hidden lg:block">
-                Visit our showroom in 1234 Street Address City Address, 1234
-              </h1>
-              <a href="#" className="border-b border-white line-clamp-1">
-                Contact Us
-              </a>
-            </div>
-            <div className="hidden lg:block">
-              <div className="flex items-center text-xs space-x-2">
-                <h1 className="font-medium">Call Us: (00) 1234 5678</h1>
-                <RiFacebookBoxFill className="w-5 h-5 text-white" />
-                <FiInstagram className="w-5 h-5 text-white" />
-              </div>
-            </div>
-          </div>
-        </section>
+        <AboutCompanySchedule
+          isSchedule={isSchedule}
+          setIsSchedule={setIsSchedule}
+        />
 
         {/* This is the Main header */}
-        <section className="bg-color-3 lg:bg-white/90 border-b border-transparent lg:border-b-gray-200 px-4 py-3">
+        <section className="bg-color-3 lg:bg-white/90 lg:hover:bg-white border-b border-transparent lg:border-b-gray-200 px-4 py-3 transition ease-in-out duration-150">
           <div className="container">
-            {/* Main Logo */}
+            {/* ====
+             * Main Logo
+             * HIDE IN LARGE SCREEN
+             * =====
+             */}
             <div className="block lg:hidden">
               <button className="absolute top-2 bg-color-3 rounded-t-[50px] p-1 active:scale-95 transition ease-in-out duration-100">
                 <a href="#">
@@ -87,18 +65,24 @@ const Header: React.FC = () => {
                 </button>
                 {!isSearch && (
                   <nav className="hidden lg:block ml-14">
-                    <ul className="flex flex-wrap items-center space-x-6 py-1.5">
+                    <div className="flex flex-wrap items-center space-x-6 py-1.5">
                       {sidebarLinks.map((link, i) => (
-                        <li key={i}>
-                          <a
-                            href="#"
-                            className="text-black font-semibold text-sm hover:text-color-3 transition ease-in-out duration-150"
-                          >
-                            {link.title}
-                          </a>
-                        </li>
+                        <div key={i} className="relative group">
+                          <div className="flex items-center space-x-1.5">
+                            <a
+                              href="#"
+                              className="text-black font-semibold text-sm group hover:text-color-3 transition ease-in-out duration-150"
+                            >
+                              {link.title}
+                            </a>
+                            {link?.submenu && (
+                              <BiChevronDown className="w-4 h-4 group-hover:text-color-3" />
+                            )}
+                          </div>
+                          <LinkDropdown link={link} />
+                        </div>
                       ))}
-                      <li className="mx-4 md:mx-6">
+                      <div className="mx-4 md:mx-6">
                         <button
                           className="px-4 py-1 text-sm rounded-full border-2 font-medium border-color-3 
                         text-color-3 hover:bg-color-3 hover:text-white
@@ -106,8 +90,8 @@ const Header: React.FC = () => {
                         >
                           Our Deals
                         </button>
-                      </li>
-                    </ul>
+                      </div>
+                    </div>
                   </nav>
                 )}
               </div>
@@ -194,6 +178,63 @@ const Header: React.FC = () => {
       {/* This is where sidebar shows */}
       <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
     </React.Fragment>
+  )
+}
+
+function AboutCompanySchedule({ isSchedule, setIsSchedule }) {
+  return (
+    <section className="text-xs font-semibold bg-black text-white">
+      <div className="container flex items-center justify-between px-4 lg:px-0 py-1">
+        <div className="ml-16 lg:ml-0">
+          <button
+            onClick={() => setIsSchedule((prev) => !prev)}
+            className="inline-flex items-center rounded-full px-1 py-0.5 border focus:border-color-5
+                  border-transparent hover:border-color-5 space-x-1 transition ease-in-out duration-150"
+          >
+            <span className="text-color-5 line-clamp-1">Mon-Thu:</span>
+            <span className="line-clamp-1">9:00 AM - 5:30 PM</span>
+            <BiChevronDown className="w-4 h-4" />
+          </button>
+          {/* This is where SchduleMenuToggle shows */}
+          {isSchedule && <ScheduleDropdown setIsSchedule={setIsSchedule} />}
+        </div>
+        <div className="flex items-center font-medium space-x-2">
+          <h1 className="text-[#ACACAC] hidden lg:block">
+            Visit our showroom in 1234 Street Address City Address, 1234
+          </h1>
+          <a href="#" className="border-b border-white line-clamp-1">
+            Contact Us
+          </a>
+        </div>
+        <div className="hidden lg:block">
+          <div className="flex items-center text-xs space-x-2">
+            <h1 className="font-medium">Call Us: (00) 1234 5678</h1>
+            <RiFacebookBoxFill className="w-5 h-5 text-white" />
+            <FiInstagram className="w-5 h-5 text-white" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+function LinkDropdown({ link }) {
+  return (
+    <div className="fixed z-10 hidden group-hover:block max-w-[230px] w-full">
+      <ul className="shadow-xl bg-white text-sm py-1">
+        {link.submenu?.map((l, i) => (
+          <li
+            key={i}
+            className="hover:bg-color-7 hover:text-color-3 transition ease-in-out duration-100"
+          >
+            <a href="#" className="flex items-center justify-between py-2 px-6">
+              <span className="font-medium">{l.title}</span>
+              <BiChevronRight className="w-4 h-4" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
