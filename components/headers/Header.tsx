b9d0606d4e06576dca466b168c85a590aedcfd8d
acatzk <joshuaimalay@gmail.com>
2021-07-29 11:37:47 +0800

    🐛 BUG(fix): Dropdown menu using headlessui (#15)

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,14 +1,12 @@
 import dynamic from 'next/dynamic'
-import { motion } from 'framer-motion'
 import React, { useState } from 'react'
+import ScheduleMenu from './ScheduleMenu'
 import { sidebarLinks } from 'mock/index'
 import { FiInstagram } from 'react-icons/fi'
 import { RiCloseLine } from 'react-icons/ri'
-import { Transition } from '@headlessui/react'
-import AccountDropdown from './AccountDropdown'
-import ScheduleDropdown from './ScheduleDropdown'
+import UserAccountMenu from './UserAccountMenu'
 import { RiFacebookBoxFill } from 'react-icons/ri'
-import { Logo, Menu, Cart, User, LogoWide } from 'utils/Icons'
+import { Logo, Menu as MenuIcon, Cart, LogoWide } from 'utils/Icons'
 import { BiChevronDown, BiSearch, BiChevronRight } from 'react-icons/bi'
 
 const Sidebar = dynamic(() => import('./Sidebar'), {
@@ -18,20 +16,36 @@ const Sidebar = dynamic(() => import('./Sidebar'), {
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [isSearch, setIsSearch] = useState(false)
-  const [isAccount, setIsAccount] = useState(false)
-  const [isSchedule, setIsSchedule] = useState(false)
 
   return (
     <React.Fragment>
-      <header className="sticky top-0 overflow-x-hidden transition-all ease-in-out duration-300 z-10">
+      <header className="sticky top-0 transition-all ease-in-out duration-300 z-10">
         {/* This is schedule and contact us links */}
-        <AboutCompanySchedule
-          isSchedule={isSchedule}
-          setIsSchedule={setIsSchedule}
-        />
+        <section className="relative text-xs font-semibold bg-black text-white">
+          <div className="container flex items-center justify-between px-4 lg:px-0 py-1">
+            <div className="ml-16 lg:ml-0 relative">
+              <ScheduleMenu />
+            </div>
+            <div className="flex items-center font-medium space-x-2">
+              <h1 className="text-[#ACACAC] hidden lg:block">
+                Visit our showroom in 1234 Street Address City Address, 1234
+              </h1>
+              <a href="#" className="border-b border-white line-clamp-1">
+                Contact Us
+              </a>
+            </div>
+            <div className="hidden lg:block">
+              <div className="flex items-center text-xs space-x-2">
+                <h1 className="font-medium">Call Us: (00) 1234 5678</h1>
+                <RiFacebookBoxFill className="w-5 h-5 text-white" />
+                <FiInstagram className="w-5 h-5 text-white" />
+              </div>
+            </div>
+          </div>
+        </section>
 
         {/* This is the Main header */}
-        <section className="bg-color-3 lg:bg-white/90 lg:hover:bg-white border-b border-transparent lg:border-b-gray-200 px-4 py-3 transition ease-in-out duration-150">
+        <section className="bg-color-3 lg:bg-white/75 lg:backdrop-blur-md lg:hover:bg-white border-b border-transparent lg:border-b-gray-200 px-4 py-3 transition ease-in-out duration-150">
           <div className="container">
             {/* ====
              * Main Logo
@@ -61,7 +75,7 @@ const Header: React.FC = () => {
                   onClick={() => setIsOpen(true)}
                   className="block lg:hidden active:scale-95 transition ease-in-out duration-100 p-1"
                 >
-                  <Menu className="w-6 h-6 text-white" />
+                  <MenuIcon className="w-6 h-6 text-white" />
                 </button>
                 {!isSearch && (
                   <nav className="hidden lg:block ml-14">
@@ -79,7 +93,7 @@ const Header: React.FC = () => {
                               <BiChevronDown className="w-4 h-4 group-hover:text-color-3" />
                             )}
                           </div>
-                          <LinkDropdown link={link} />
+                          <SubMenuDropdown link={link} />
                         </div>
                       ))}
                       <div className="mx-4 md:mx-6">
@@ -154,22 +168,16 @@ const Header: React.FC = () => {
                   )}
                 </div>
                 {/* Cart button */}
-                <button className="relative right-1 md:right-0 top-1 flex items-center active:scale-95 transition ease-in-out duration-100">
-                  <div className="absolute text-[0.65rem] px-1.5 py-0.5 font-bold bg-white lg:bg-color-3 text-color-3 lg:text-white rounded-full -top-3.5 left-3.5">
-                    2
-                  </div>
-                  <Cart className="w-5 h-5 text-white lg:text-black" />
-                </button>
-                {/* User dropdown button */}
                 <div>
-                  <button
-                    onClick={() => setIsAccount((prev) => !prev)}
-                    className="flex items-center justify-center p-2 border-2 border-white lg:border-black rounded-full active:scale-95 transition ease-in-out duration-100"
-                  >
-                    <User className="w-3 h-3 text-white lg:text-black" />
+                  <button className="relative right-1 md:right-0 top-1 flex items-center active:scale-95 transition ease-in-out duration-100">
+                    <div className="absolute text-[0.65rem] px-1.5 py-0.5 font-bold bg-white lg:bg-color-3 text-color-3 lg:text-white rounded-full -top-3.5 left-3.5">
+                      2
+                    </div>
+                    <Cart className="w-5 h-5 text-white lg:text-black" />
                   </button>
-                  {isAccount && <AccountDropdown setIsAccount={setIsAccount} />}
                 </div>
+                {/* User Account Menu dropdown button */}
+                <UserAccountMenu />
               </div>
             </div>
           </div>
@@ -181,44 +189,7 @@ const Header: React.FC = () => {
   )
 }
 
-function AboutCompanySchedule({ isSchedule, setIsSchedule }) {
-  return (
-    <section className="text-xs font-semibold bg-black text-white">
-      <div className="container flex items-center justify-between px-4 lg:px-0 py-1">
-        <div className="ml-16 lg:ml-0">
-          <button
-            onClick={() => setIsSchedule((prev) => !prev)}
-            className="inline-flex items-center rounded-full px-1 py-0.5 border focus:border-color-5
-                  border-transparent hover:border-color-5 space-x-1 transition ease-in-out duration-150"
-          >
-            <span className="text-color-5 line-clamp-1">Mon-Thu:</span>
-            <span className="line-clamp-1">9:00 AM - 5:30 PM</span>
-            <BiChevronDown className="w-4 h-4" />
-          </button>
-          {/* This is where SchduleMenuToggle shows */}
-          {isSchedule && <ScheduleDropdown setIsSchedule={setIsSchedule} />}
-        </div>
-        <div className="flex items-center font-medium space-x-2">
-          <h1 className="text-[#ACACAC] hidden lg:block">
-            Visit our showroom in 1234 Street Address City Address, 1234
-          </h1>
-          <a href="#" className="border-b border-white line-clamp-1">
-            Contact Us
-          </a>
-        </div>
-        <div className="hidden lg:block">
-          <div className="flex items-center text-xs space-x-2">
-            <h1 className="font-medium">Call Us: (00) 1234 5678</h1>
-            <RiFacebookBoxFill className="w-5 h-5 text-white" />
-            <FiInstagram className="w-5 h-5 text-white" />
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
-
-function LinkDropdown({ link }) {
+function SubMenuDropdown({ link }) {
   return (
     <div className="fixed z-10 hidden group-hover:block max-w-[230px] w-full">
       <ul className="shadow-xl bg-white text-sm py-1">
